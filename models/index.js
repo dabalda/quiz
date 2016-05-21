b4de44386a8561ae7690062b3193e7612d5d3ba2
David Baldazo <d.baldazo@gmail.com>
2016-05-21 14:41:34 +0200

    Tema 27: Subir imágenes

--- models/index.js
+++ models/index.js
@@ -26,25 +26,32 @@ var sequelize = new Sequelize(url,
 				              });
 
 
-// Importar la definicion de la tabla Quiz de quiz.js
+// Importar la definición de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-// Importar la definicion de la tabla Comments de comment.js
+// Importar la definición de la tabla Comments de comment.js
 var Comment = sequelize.import(path.join(__dirname,'comment'));
 
-// Importar la definicion de la tabla Users de user.js
+// Importar la definición de la tabla Users de user.js
 var User = sequelize.import(path.join(__dirname,'user'));
+
+// Importar la definición de la tabla Attachments de attachment.js
+var Attachment = sequelize.import(path.join(__dirname,'attachment'));
 
 
 // Relaciones entre modelos
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
-// Relacion 1 a N entre User y Quiz:
+// Relación 1 a N entre User y Quiz:
 User.hasMany(Quiz, {foreignKey: 'AuthorId'});
 Quiz.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'}); // Añade Author a quiz en lugar de AuthorId
 
+// Relación 1 a 1 ente Quiz y Attachment
+Attachment.belongsTo(Quiz);
+Quiz.hasOne(Attachment);
 
-exports.Quiz = Quiz;       // exportar definición de tabla Quiz
-exports.Comment = Comment; // exportar definición de tabla Comments
-exports.User = User;       // exportar definición de tabla Users
+exports.Quiz = Quiz;       			// exportar definición de tabla Quiz
+exports.Comment = Comment; 			// exportar definición de tabla Comments
+exports.User = User;       			// exportar definición de tabla Users
+exports.Attachment = Attachment; 	// exportar definición de tabla Attachments
